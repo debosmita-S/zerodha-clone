5ff7b15ae11f528c235bb178cc1807ef8ccb4c5e
Debosmita Samanta <debosmitasamanta23@gmail.com>
2025-06-29 08:37:13 +0530

    Fix: Removed Award.js

--- frontend/src/landing_page/home/Awards.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-function Awards() {
-    return ( 
-        Awards
-     );
-}
-
-export default Awards;
--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Brokerage from './Brokerage';
 import Hero from './Hero';
 import Charges from './Charges';
-import OpenAccount from '../OpenAccount';
+// import OpenAccount from '../OpenAccount';
 
 function PricingPage() {
     return (
